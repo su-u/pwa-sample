b4e74d3b2c7ce466193bfb813197939b0e485d04
su- <aaaiiiuuu35@gmail.com>
2020-06-15 17:13:17 +0900

    pwa

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Root from './Root';
 import createFinalStore from './stores/createFinalStore';
+import * as serviceWorker from './serviceWorker';
 
 const store = createFinalStore();
 
 ReactDOM.render(<Root store={store} />, document.getElementById('root'));
+serviceWorker.register();
